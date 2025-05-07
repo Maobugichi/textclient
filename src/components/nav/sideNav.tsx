fdd3505b552f3bba90e2260afde39ce7df5137a8
Maobugichi <immansamuel846@gmail.com>
2025-05-07 16:03:22 -0700

    sisisi22

--- src/components/nav/sideNav.tsx
+++ src/components/nav/sideNav.tsx
@@ -43,7 +43,7 @@ const SideNav:React.FC<SideNavProps> = ({show , setIsShow}) => {
                        <X onClick={closeNav} className="md:hidden"/>
                    </div>
                    
-                   <div className=" h-[70%]">
+                   <div className=" h-[90%]">
                         <NavItems
                         closeNav={closeNav}
                         />
