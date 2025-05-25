069e2374ec5b054ad5ac626cba5d9531079d356e
Maobugichi <immansamuel846@gmail.com>
2025-05-24 21:00:26 -0700

    update input again9

--- src/ui/hero.tsx
+++ src/ui/hero.tsx
@@ -16,13 +16,13 @@ const Hero = () => {
                     <h1 className="text-4xl w-full md:text-6xl md:w-[95%] font-bold">Premium Virtual SMS Authentication Solution</h1>
                     <p className="text-gray-400 w-[95%] leading-8 text-lg">Get SMS messages online with trusted virtual phone numbers from around the globe. Our platform offers fast, secure, and dependable SMS verification for any purpose.</p>
                     <div className="w-[95%]  flex">
-                       <Link  className="bg-[#0032a5] w-[40%] md:w-[23%] h-11 rounded-sm grid place-content-center text-white text-sm" to="signup/:1">
+                       <Link  className="bg-[#0032a5] w-[40%] md:w-[23%] h-11 rounded-sm grid place-content-center text-white text-sm" to="/signup/:1">
                         <Button
                          content="Sign up"
                         
                         />
                         </Link> 
-                        <Link className="md:w-[23%] w-[40%] h-11 text-sm grid place-content-center" to="login/:1">
+                        <Link className="md:w-[23%] w-[40%] h-11 text-sm grid place-content-center" to="/login/:1">
                             <Button
                              content={<span className="flex w-full items-center gap-2 justify-center">Login <ArrowRight size={13}/></span>}
                             />
