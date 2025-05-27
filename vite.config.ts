a99ce9820dff3ebdb2f8810dabeb92bacde8ec19
Maobugichi <immansamuel846@gmail.com>
2025-05-27 10:06:34 -0700

    edit api route5

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,13 @@ export default defineConfig({
   },
   base:'/',
   plugins: [react(),tailwindcss()],
- 
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true, 
+        drop_debugger: true
+      }
+    } 
+  }
 })
