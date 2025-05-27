6a015db64487aaaf389dfb3975e70e139497139c
Maobugichi <immansamuel846@gmail.com>
2025-05-27 09:56:36 -0700

    edit api route3

--- src/ui/hero-img.tsx
+++ src/ui/hero-img.tsx
@@ -3,7 +3,7 @@ import Skeleton from "react-loading-skeleton"
 const HeroImg = () => {
     return(
         <div className="h-[40vh] w-full md:w-1/2 md:h-[90vh] relative md:right-[-120px] rounded-lg p-1 border border-solid border-blue-400 overflow-hidden">
-           {heroImg ? <img className="h-full object-fill" src={heroImg} alt="" /> :  <Skeleton height='90vh' baseColor="#e0e0e0" highlightColor="#f5f5f5" /> }
+           {heroImg ? <img className="h-full object-cover" src={heroImg} alt="" /> :  <Skeleton height='90vh' baseColor="#e0e0e0" highlightColor="#f5f5f5" /> }
             <div className="absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-t from-gray-200 to-transparent pointer-events-none" />
         </div>
     )
