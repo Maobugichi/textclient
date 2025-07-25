38fa31840d6a346814e606c59f747f393a6660b4
Maobugichi <immansamuel846@gmail.com>
2025-07-25 09:25:40 -0700

    test

--- src/components/payment.tsx
+++ src/components/payment.tsx
@@ -56,8 +56,8 @@ const Payment = () => {
     const { id ,email , amount , currency } = data;
     if (id !== '' && email !== '' && amount !== '' && currency !== '') {
       if (amount < 1000) {
-        setErr(true)
-        return
+        //setErr(true)
+        //return
       }
       
       setShowLoader(true)
