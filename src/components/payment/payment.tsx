b80878b7cf1e3f3fcb4305fa52297f616946a918
Maobugichi <immansamuel846@gmail.com>
2025-10-21 14:48:07 +0100

    update validation

--- src/components/payment/payment.tsx
+++ src/components/payment/payment.tsx
@@ -41,8 +41,8 @@ const Payment = () => {
     const { id ,email , amount , currency } = data;
     if (id !== '' && email !== '' && amount !== '' && currency !== '') {
       if (amount < 1000) {
-        //setErr(true)
-        //return
+        setErr(true)
+        return
       }
       
       setShowLoader(true)
